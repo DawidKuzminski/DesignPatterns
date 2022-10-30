1ee624371271f208e3375ccee8b8690e2055517c
Dawid Kuźmiński <d.kuzminski@outlook.com>
2022-10-30 15:06:57 +0100

    Creational design pattern Singleton added

---
+++ CreationalDesignPattern-Singleton/Configuration.cs
@@ -0,0 +1,31 @@
+﻿namespace CreationalDesignPattern_Singleton;
+internal class Configuration
+{
+	private static Configuration _instance;
+	private static readonly object _instanceLock = new object();
+
+	public string InitValue { get; set; }
+
+	private Configuration() 
+	{
+		InitValue = "Hello Singleton!";
+	}
+
+	public static Configuration Instance 
+	{
+		get 
+		{
+			if(_instance is null)
+			{
+				lock(_instanceLock)
+				{
+					if(_instance is null)
+					{
+						_instance = new Configuration();
+					}
+				}
+			}
+			return _instance; 
+		}
+	}
+}
---
+++ CreationalDesignPattern-Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CreationalDesignPattern_Singleton;
+
+Console.WriteLine("Creational Design Pattern: Singleton!");
+
+var config = Configuration.Instance;
+Console.WriteLine($"Created instance value: {config.InitValue}");
+
+config.InitValue = "Created instance value after changed";
+Console.WriteLine(config.InitValue);
+
+var config2 = Configuration.Instance;
+Console.WriteLine($"Another instace value after init: {config.InitValue}");
+
+Console.ReadKey();
+
+
+
